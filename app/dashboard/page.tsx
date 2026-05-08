a8112137ceab856747439b243444a42296c5eca9
celularairton3b2-cpu <celularairton3b2@gmail.com>
2026-05-07 23:48:57 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,1 +1,14 @@
+// app/dashboard/page.tsx
+'use client'
 
+import { ListaRefeicoes } from '@/components/lista-refeicoes'
+import { AdicionarRefeicao } from '@/components/adicionar-refeicao'
+
+export default function DashboardPage() {
+  return (
+    <>
+      <ListaRefeicoes />
+      <AdicionarRefeicao />
+    </>
+  )
+}
